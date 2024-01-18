285842d3283889966684e9d2f8425400c2cc4db4
Prince Boateng Obeng <110475124+nanakwamefamous@users.noreply.github.com>
2024-01-18 15:39:44 +0000

    update

--- online-work-africa/src/Pages/Sponsors.jsx
+++ online-work-africa/src/Pages/Sponsors.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 import "../styles/Sponsors.css"
 import sponsorsimg_1 from '../images/LOGO 1.png'
 import sponsorsimg_2 from '../images/LOGO 2.png'
-import sponsorsimg_4 from '../images/LOGO 4.jpg'
-import sponsorsimg_5 from '../images/LOGO 5.jpg'
-import sponsorsimg_6 from '../images/LOGO 6.jpg'
-import sponsorsimg_7 from '../images/LOGO 7.png'
-import sponsorsimg_8 from '../images/LOGO 8.png'
 import PartnerWithUs from '../components/PartnerWithUs';
 import Aos from "aos";
 import "aos/dist/aos.css";
@@ -29,11 +24,6 @@ export default function Sponsors() {
           <div className='spimg'>
             <img data-aos="zoom-in" src={sponsorsimg_1} alt="" />
             <img data-aos="zoom-in" src={sponsorsimg_2} alt="" />
-            <img data-aos="zoom-in" src={sponsorsimg_4} alt="" />
-            <img data-aos="zoom-in" src={sponsorsimg_5} alt="" />
-            <img data-aos="zoom-in" src={sponsorsimg_6} alt="" />
-            <img data-aos="zoom-in" src={sponsorsimg_7} alt="" />
-            <img data-aos="zoom-in" src={sponsorsimg_8} alt="" />
           </div>
         </div>
         <PartnerWithUs />
